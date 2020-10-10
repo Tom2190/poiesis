e460c28adbf54cb07b265e75c22a42c29ba973db
Priscila Bey <pri.bey@gmail.com>
2020-10-10 15:35:50 -0300

    inscripcion styles and html

---
+++ poiesisApi/poiesis/poiesis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace poiesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
